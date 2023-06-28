30dc07f42b03e18770aaf84f53a1dde198f7a213
SIRSoares33 <sirgustavo4@gmail.com>
2023-06-28 13:15:07 -0300

    Criando a primeira classe.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Jogo_de_xadrez;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Posicao posicao = new Posicao(2, 3);
+        
+        Console.WriteLine(posicao);
+        Console.ReadKey();
+    }
+}
---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,19 @@
+namespace Tabuleiro
+{
+    public class Posicao
+    {
+        public int Linha { get; protected set; }
+        public int Coluna { get; protected set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"Posição: ({Linha}, {Coluna})";
+        }
+    }
+}
